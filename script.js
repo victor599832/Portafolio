f8070b48f4dad3f52ec2fd0b4272c337461c97a9
Victor <anifan599@gmail.com>
2025-08-28 00:50:29 -0600

    6 actualizacion

--- script.js
+++ script.js
@@ -1,39 +1,66 @@
 const form = document.getElementById('Formulario');
+let campos = document.querySelectorAll('.campos');
+let Alerta = document.getElementById("Alerta");
 document.querySelector('Textarea').value = '';
 
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    let Nombre = document.getElementById('Nombre').value;
-    let Correo = document.getElementById('Email').value;
-    let Asunto = document.getElementById('Asunto').value;
-    let Mensaje = document.getElementById('Textarea').value;
-    if (Nombre === '') {
-        alert('Por favor, ingresa un nombre válido');
-        return;
-    }
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    let valido = true;
+    campos.forEach(campo => {
+        let tipo = campo.dataset.name;
+        let error__name = campo.dataset.error;
+        let error = document.querySelector(error__name);
 
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(Correo)) {
-      alert('Por favor, ingresa un correo válido');
-      return;
-    }
+        switch (tipo) {
+            case 'nombre':
+                    if (campo.value === "") {
+                        error.style.display = 'block';
+                        valido = false;
+                    } else {
+                        error.style.display = 'none';
+                    }
+                break;
+            case 'email':
+                const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+                if (!emailRegex.test(campo.value)) {
+                    error.style.display = 'block';
+                        valido = false;
+                    } else {
+                        error.style.display = 'none';
+                    }
+                break;
+                case 'asunto':
+                if (campo.value === "") {
+                        error.style.display = 'block';
+                        valido = false;
+                    } else {
+                        error.style.display = 'none';
+                    }
+                break;
+                case 'mensaje':
+                if (campo.value === "") {
+                        error.style.display = 'block';
+                        valido = false;
+                    } else {
+                        error.style.display = 'none';
+                    }
+                break;
+            default:
+                break;
+        }
+    });
 
-     if (Asunto === '') {
-        alert('Por favor, ingresa un asunto válido');
-        return;
-    }
 
-    if (Mensaje.length < 6 ) {
-        alert('El mensaje debe tener almenos 6 Caracteres');
-        return;
-    }
-
-    emailjs.sendForm('service_lowo9bb','template_6sywnnn', this).then(() =>{
-        alert('¡Mensaje enviado con éxito!');
+    if (valido) {
+        Alerta.classList.add('activa');
+        emailjs.sendForm('service_lowo9bb','template_6sywnnn', this).then(() =>{
+        setInterval(() => {
+            Alerta.classList.remove('activa');
+        }, 1000);
             form.reset();
         }, (err) => {
             alert('Error al enviar el mensaje: ' + JSON.stringify(err));
     });
-   
+    }
 });
 
